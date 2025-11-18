fb85a465e970f15498b0a2c1b70c0f349fd2aae2
Samreen Fatima <samreeenfatima11@gmail.com>
2025-11-18 23:42:19 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,16 @@
+// let num=prompt("enter any number")
+// console.log(num+=4)
+// console.log(num-=4)
+// console.log(num*=4)
+// console.log(num/=4)
+// console.log(num%=4)
+// console.log(num)
+
+
+var date=new Date();
+// console.log(date.toLocaleTimeString())
+console.log(date.toLocaleDateString())
+
+let dayname=['sunday','monday' ,'tuesday' , 'wednesday' ,'thursday' ,'friday' ,'saturday']
+let dayvalue=new Date().getDate()
+console.log(dayname(dayvalue))
