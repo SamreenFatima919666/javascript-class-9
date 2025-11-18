7990771dc1d75de0ec85c5e530847a3dc0af58f9
Samreen Fatima <samreeenfatima11@gmail.com>
2025-11-18 23:51:00 +0500

    Update app.js

--- app.js
+++ app.js
@@ -24,15 +24,27 @@
 // getDay() => current day but in range format 0-6 , 0 means sunday
 
 let currentDate = new Date();
-console.log(currentDate);
+// console.log(currentDate);
 
-console.log('Current Day: '+currentDate.getDay());//0-6 0-sun
-console.log('Current Month: '+currentDate.getMonth());//0-11 0-jan
-console.log('Current Date: '+currentDate.getDate());//1-31
-console.log('Current Year: '+currentDate.getFullYear());//no range 
-console.log('Current Hour: '+currentDate.getHours());//0-23 0-12:am 23-11:pm
-console.log('Current Mins: '+currentDate.getMinutes());//0-59 0- 1 min
-console.log('Current Secs: '+currentDate.getSeconds());//0-59 0-1sec
-console.log('Current MilliSecs: '+currentDate.getMilliseconds());//0-999 1 sec = 1000millisec
-console.log(currentDate.getTime());
-console.log(currentDate.getTimezoneOffset());//timezone offset
+// console.log('Current Day: '+currentDate.getDay());//0-6 0-sun
+// console.log('Current Month: '+currentDate.getMonth());//0-11 0-jan
+// console.log('Current Date: '+currentDate.getDate());//1-31
+// console.log('Current Year: '+currentDate.getFullYear());//no range 
+// console.log('Current Hour: '+currentDate.getHours());//0-23 0-12:am 23-11:pm
+// console.log('Current Mins: '+currentDate.getMinutes());//0-59 0- 1 min
+// console.log('Current Secs: '+currentDate.getSeconds());//0-59 0-1sec
+// console.log('Current MilliSecs: '+currentDate.getMilliseconds());//0-999 1 sec = 1000millisec
+// console.log(currentDate.getTime());
+// console.log(currentDate.getTimezoneOffset());//timezone offset
+
+document.writeln(currentDate);
+document.writeln('Current Day: '+currentDate.getDay());//0-6 0-sun
+document.writeln('Current Month: '+currentDate.getMonth());//0-11 0-jan
+document.writeln('Current Date: '+currentDate.getDate());//1-31
+document.writeln('Current Year: '+currentDate.getFullYear());//no range 
+document.writeln('Current Hour: '+currentDate.getHours());//0-23 0-12:am 23-11:pm
+document.writeln('Current Mins: '+currentDate.getMinutes());//0-59 0- 1 min
+document.writeln('Current Secs: '+currentDate.getSeconds());//0-59 0-1sec
+document.writeln('Current MilliSecs: '+currentDate.getMilliseconds());//0-999 1 sec = 1000millisec
+document.writeln(currentDate.getTime());
+document.writeln(currentDate.getTimezoneOffset());//timezone offset
